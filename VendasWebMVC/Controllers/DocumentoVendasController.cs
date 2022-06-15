75c5af685013ef2648d2b6259ae2cf4167826231
Leandro Machado <leoantunes.fm@gmail.com>
2022-06-15 01:11:51 -0300

    Inclusão de novos menus

---
+++ VendasWebMVC/Controllers/DocumentoVendasController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VendasWebMVC.Controllers
+{
+    public class DocumentoVendasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult BuscaSimples()
+        {
+            return View();
+        }
+
+        public IActionResult BuscaAgrupada()
+        {
+            return View();
+        }
+    }
+}
